ce5611493959c1c55ff2944c42dffb1f696284c8
Naveenbobburi <naveenbobburi60@gmail.com>
2025-07-28 21:23:15 +0530

    editing header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 
 const App = () => {
   return (
     <div class = "App">
       <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/cart"  element = {<Cart/>} />
+        <Route path = "/placeorder" element = {<PlaceOrder/>} />
+      </Routes>
     </div>
   )
 }
 
 export default App
+
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from 'react-dom/client'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const TestMessage = () => <div style={{ color: 'red' }}>If you see this, React is rendering.</div>;
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <TestMessage />
     <App />
-  </StrictMode>,
+  </BrowserRouter>
 )
