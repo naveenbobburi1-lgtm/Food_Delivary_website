426dc54a154d4a2e23a912f9b74d359065137f00
Naveenbobburi <naveenbobburi60@gmail.com>
2025-08-18 22:25:57 +0530

    Updating the menu

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import './FoodDisplay.css'
 import { StoreContext } from '../../context/StoreContext'
+import FoodItem from '../FoodItem/FoodItem'
 
 const FoodDisplay = ({category}) => {
     const { food_list } = useContext(StoreContext)
@@ -10,8 +11,20 @@ const FoodDisplay = ({category}) => {
         : food_list.filter(item => item.category === category)
 
     return (
-        <div className='food-display' id='food-display'>
+        <div className='food-display' id = 'food-display'>
             <h2>Top Dishes Near You</h2>
+            <div className="food-display-list">
+                {filteredFoodList.map((item,index) => (
+                    <FoodItem 
+                        key={index}
+                        id={item._id}
+                        name={item.name}
+                        description={item.description}
+                        price={item.price}
+                        image={item.image}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
